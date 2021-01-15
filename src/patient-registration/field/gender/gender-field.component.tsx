5965717f0a7a8a5acd042ef1bc0b2366428a950c
Ivange  Larry <ivangelarry@gmail.com>
2021-01-15 16:37:54 +0100

    Attemt to fixing datepicker

--- src/patient-registration/field/gender/gender-field.component.tsx
+++ src/patient-registration/field/gender/gender-field.component.tsx
@@ -11,7 +11,7 @@ export const GenderField = () => {
       <h4 className={styles.productiveHeading02Light}>Sex &amp; Birth</h4>
       <div style={{ marginBottom: '1rem' }}>
         <p className="bx--label">Sex</p>
-        <RadioButtonGroup name="gender" orientation="vertical" {...field}>
+        <RadioButtonGroup name="gender" orientation="vertical">
           <RadioButton id="male" labelText="Male" value="male" />
           <RadioButton id="female" labelText="Female" value="female" />
           <RadioButton id="other" labelText="Other" value="other" />
@@ -19,13 +19,16 @@ export const GenderField = () => {
       </div>
       <br />
       <div style={{ marginBottom: '1rem' }}>
-        <DatePicker dateFormat="d/m/Y" datePickerType="single" light>
+        <DatePicker
+          dateFormat="d/m/Y"
+          datePickerType="single"
+          light
+          onChange={([value]) => field.onChange({ value, type: 'change' })}>
           <DatePickerInput
             id="birthdate"
             name="birthdate"
             placeholder="dd/mm/yyyy"
             labelText="Date of Birth"
-            {...field}
             invalid={!!(meta.touched && meta.error)}
             invalidText={meta.error}
           />
